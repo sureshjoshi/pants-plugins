75e1565227e5933289f6effd986245c9f18f4b85
SJ <suresh@vicarasolutions.com>
2022-02-02 10:16:43 -0500

    See issue #2: Committing the mypyc fib example

---
+++ hellofib/hellofib/main.py
@@ -0,0 +1,18 @@
+import time 
+
+def main():
+    def fib(n: int) -> int:
+        if n <= 1:
+            return n
+        else:
+            return fib(n - 2) + fib(n - 1)
+    
+    start_time = time.time()
+    fib(32)
+    print(f"Calculating fibs took {time.time() - start_time} seconds")
+
+
+# In .bzl config, setting python_config.run_module = "hellofib.main" should cause this to run as the entry point
+if __name__ == "__main__":
+    print("Launching HelloFib from __ main__")
+    main()

96c1298c781504850613985b83574fcf2199cf4c
SJ <suresh@vicarasolutions.com>
2022-02-13 02:24:04 -0500

    See issue #4: Created a deploy goal rule - Created a check goal for an ansible playbook (--syntax-check)

---
+++ pants-plugins/experimental/ansible/rules.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+import logging
+
+from pants.backend.python.util_rules.pex import Pex, PexProcess, PexRequest
+from pants.core.goals.check import CheckRequest, CheckResult, CheckResults
+from pants.engine.fs import Digest, RemovePrefix
+from pants.engine.process import FallibleProcessResult
+from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.target import FieldSet, HydratedSources, HydrateSourcesRequest,SingleSourceField, WrappedTarget, Address
+from pants.engine.unions import UnionRule
+from pants.util.logging import LogLevel
+
+from experimental.ansible.subsystem import Ansible
+from experimental.ansible.target_types import AnsibleDependenciesField, AnsiblePlaybook
+
+logger = logging.getLogger(__name__)
+
+@dataclass(frozen=True)
+class AnsibleFieldSet(FieldSet):
+    required_fields = (AnsibleDependenciesField, AnsiblePlaybook,)
+
+    dependencies: AnsibleDependenciesField
+    playbook: AnsiblePlaybook
+
+
+class AnsibleCheckRequest(CheckRequest):
+    field_set_type = AnsibleFieldSet
+
+
+@rule(level=LogLevel.DEBUG)
+async def ansible_check(
+    request: AnsibleCheckRequest, ansible: Ansible
+) -> CheckResults:
+    # if ansible.skip:
+        # return CheckResults([], checker_name="Ansible")
+    
+    # TODO: Pull this out into separate rule to hydrate the playbook
+    field_set: AnsibleFieldSet = request.field_sets[0]
+    logger.info(field_set)
+    wrapped_target = await Get(WrappedTarget, Address, field_set.address)
+    target = wrapped_target.target
+    sources = await Get(
+        HydratedSources,
+        HydrateSourcesRequest(
+            target.get(SingleSourceField),
+            for_sources_types=(AnsiblePlaybook,),
+        ),
+    )
+
+    # Drop the top-level directory
+    flattened_digest = await Get(Digest, RemovePrefix(sources.snapshot.digest, sources.snapshot.dirs[0]))
+
+    # Install ansible
+    ansible_pex = await Get(
+        Pex,
+        PexRequest(
+            output_filename="ansible.pex",
+            internal_only=True,
+            requirements=ansible.pex_requirements(),
+            interpreter_constraints=ansible.interpreter_constraints,
+            main=ansible.main,
+        ),
+    )
+
+    # Run the ansible syntax check on the passed-in playbook
+    process_result = await Get(
+        FallibleProcessResult,
+        PexProcess(
+            ansible_pex,
+            argv=["--syntax-check", field_set.playbook.value or field_set.playbook.default],
+            description="Running Ansible syntax check...",
+            input_digest=flattened_digest,
+            level=LogLevel.DEBUG,
+        )
+    )
+
+    return CheckResults(
+        [CheckResult.from_fallible_process_result(process_result)], checker_name="Ansible"
+    )
+
+def rules():
+    return (
+        *collect_rules(),
+        UnionRule(CheckRequest, AnsibleCheckRequest),
+    )

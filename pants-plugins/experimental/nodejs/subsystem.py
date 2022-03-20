e5e7f2f4191fccecd1c510195c4a58f7b5ddbdde
SJ <suresh@vicarasolutions.com>
2022-03-19 23:34:51 -0400

    See issue #32: Added missing architectures

--- pants-plugins/experimental/nodejs/subsystem.py
+++ pants-plugins/experimental/nodejs/subsystem.py
@@ -13,14 +13,17 @@ class NodeJS(ExternalTool):
 
     default_version = "v16.14.1"
     default_known_versions = [
+        "v16.14.1|macos_arm64|8f6d45796f3d996484dcf53bb0e53cd019cd0ef7a1a247bd0178ebaa7e63a184|28983951",
         "v16.14.1|macos_x86_64|af35abd727b051c8cdb8dcda9815ae93f96ef2c224d71f4ec52034a2ab5d8b61|30414480",
+        "v16.14.1|linux_arm64|53aeda7118cd1991424b457907790033326f432ee6c2908a7693920124622cf4|32852829",
+        "v16.14.1|linux_x86_64|8db3d6d8ecfc2af932320fb12449de2b5b76f946ac72b47c6a9074afe82737ff|32861678",
     ]
 
     PLATFORM_MAPPING = {
-        # "macos_arm64": "darwin.x86_64",
+        "macos_arm64": "darwin-arm64",
         "macos_x86_64": "darwin-x64",
-        # "linux_arm64": "linux.aarch64",
-        # "linux_x86_64": "linux.x86_64",
+        "linux_arm64": "linux-arm64",
+        "linux_x86_64": "linux-x64",
     }
 
     def generate_url(self, plat: Platform) -> str:

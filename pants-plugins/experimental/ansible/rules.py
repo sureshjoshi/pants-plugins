fa70106a057ea25798eae0b167e5aad6e7fb4ede
Daniel Goldman <danielgoldman4@gmail.com>
2022-04-06 17:40:38 -0400

    insert galaxy requirement installation in ansible deploy

--- pants-plugins/experimental/ansible/rules.py
+++ pants-plugins/experimental/ansible/rules.py
@@ -11,6 +11,7 @@ from experimental.ansible.sources import (
     AnsibleSourcesDigest,
 )
 from experimental.ansible.subsystems.ansible import Ansible
+from experimental.ansible.subsystems.ansible_galaxy import AnsibleGalaxy
 from experimental.ansible.subsystems.ansible_lint import AnsibleLint
 from experimental.ansible.target_types import AnsiblePlaybook
 from pants.backend.python.util_rules.pex import Pex, PexProcess, PexRequest
@@ -18,6 +19,7 @@ from pants.core.goals.check import CheckRequest, CheckResult, CheckResults
 from pants.core.goals.lint import LintResult, LintResults, LintTargetsRequest
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.fs import Digest, MergeDigests
+from pants.engine.internals.native_engine import EMPTY_DIGEST
 from pants.engine.process import FallibleProcessResult, ProcessCacheScope
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.target import FieldSet, HydratedSources, HydrateSourcesRequest
@@ -97,7 +99,7 @@ async def run_ansible_check(
 
 @rule(level=LogLevel.DEBUG)
 async def run_ansible_playbook(
-    request: AnsibleFieldSet, ansible: Ansible
+    request: AnsibleFieldSet, ansible: Ansible, galaxy: AnsibleGalaxy
 ) -> DeployResults:
     context_files_get = Get(
         AnsibleSourcesDigest,
@@ -114,13 +116,76 @@ async def run_ansible_playbook(
 
     # Install Ansible
     ansible_pex_get = Get(Pex, PexRequest, ansible.to_pex_request())
+    galaxy_pex_get = Get(Pex, PexRequest, galaxy.to_pex_request())
 
-    context_files, playbook, ansible_pex = await MultiGet(
-        context_files_get, playbook_get, ansible_pex_get
+    context_files, playbook, ansible_pex, galaxy_pex = await MultiGet(
+        context_files_get, playbook_get, ansible_pex_get, galaxy_pex_get
+    )
+
+    # Start Galaxy
+
+    galaxy_requirements_digest = EMPTY_DIGEST
+    if galaxy.requirements:
+        # Install any top-level Galaxy requirements
+        galaxy_requirements_process_result = await Get(
+            FallibleProcessResult,
+            PexProcess(
+                galaxy_pex,
+                argv=(
+                    "collection",
+                    "install",
+                    "-r",
+                    galaxy.requirements,
+                    "-p",
+                    galaxy.collections_path,
+                ),
+                description=f"Installing ansible-galaxy from {galaxy.requirements}",
+                input_digest=context_files.digest,
+                output_directories=(galaxy.collections_path,),
+                level=LogLevel.DEBUG,
+                cache_scope=ProcessCacheScope.PER_RESTART_SUCCESSFUL,
+            ),
+        )
+        galaxy_requirements_digest = galaxy_requirements_process_result.output_digest
+
+    galaxy_collections_digest = EMPTY_DIGEST
+    if galaxy.collections:
+        # Install any top-level Galaxy collections
+        galaxy_process_result = await Get(
+            FallibleProcessResult,
+            PexProcess(
+                galaxy_pex,
+                argv=(
+                    "collection",
+                    "install",
+                    *galaxy.collections,
+                    "-p",
+                    galaxy.collections_path,
+                ),
+                description="Installing ansible-galaxy collections",
+                output_directories=(galaxy.collections_path,),
+                level=LogLevel.DEBUG,
+                cache_scope=ProcessCacheScope.PER_RESTART_SUCCESSFUL,
+            ),
+        )
+        galaxy_collections_digest = galaxy_process_result.output_digest
+
+    # End Galaxy
+
+    # Combine Galaxy dependencies and Playbook context
+    merged_digest = await Get(
+        Digest,
+        MergeDigests(
+            [
+                context_files.digest,
+                galaxy_requirements_digest,
+                galaxy_collections_digest,
+            ]
+        ),
     )
 
     playbook_filename = playbook.snapshot.files[0]
-    argv = [playbook_filename]
+    argv = [playbook_filename, *ansible.args]
     argv.extend(request.ansible_playbook_args.value)
     # Run the passed-in playbook
     process_result = await Get(
@@ -129,7 +194,7 @@ async def run_ansible_playbook(
             ansible_pex,
             argv=argv,
             description="Running Ansible Playbook...",
-            input_digest=context_files.digest,
+            input_digest=merged_digest,
             level=LogLevel.DEBUG,
             cache_scope=ProcessCacheScope.PER_RESTART_SUCCESSFUL,
         ),
@@ -137,7 +202,7 @@ async def run_ansible_playbook(
 
     return DeployResults(
         [DeployResult.from_fallible_process_result(process_result)],
-        deployer_name="Ansible",
+        deployer_name=Ansible.options_scope,
     )
 
 
